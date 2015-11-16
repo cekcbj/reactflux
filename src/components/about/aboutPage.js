000f5d866be8f3cd950e8b2fb95908e6572f436e
Byronj <cekcbj@gmail.com>
2015-11-16 07:46:56 -0600

    simple routing/ added about

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1> About </h1>
+        <p>
+          This app uses react, flux and react react-router
+          </p>
+      </div>
+    );
+  }
+});
+
+module.exports = About;
---
+++ src/components/homepage.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div className="jumbotron">
+        <h1> Trainer Administration</h1>
+        <p> react,react router, and flux</p>
+      </div>
+    );
+  }
+});
+
+module.exports = Home;
